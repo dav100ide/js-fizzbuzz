c02e911e89139ab224cd440e942fe74db2b5ca8e
dav100ide <oreglio.dev@gmail.com>
2022-10-24 16:56:42 +0200

    css layout

--- js/main.js
+++ js/main.js
@@ -1,19 +1,22 @@
 // ciclo a contatore, 100 volte a partire da 1
 for (let i = 1; i <= 100; i++) {
    // stampo i numeri da 1 a 100; ma i multipli di 3 = "fizz" & i multiplo di 5 = "buzz" & i multipli di 15 = "fizzBuzz"
-   let shownValue = i;
-   if (i % 3 === 0 && i % 5 === 0) {
-      shownValue = 'FizzBuzz';
-   } else if (i % 3 === 0) {
-      shownValue = 'Fizz';
-   } else if (i % 5 === 0) {
-      shownValue = 'Buzz';
-   }
    // creo tanti box quanti sono sono gli shownValue
    const fizzBuzzBox = document.querySelector('.fizz-buzz-box');
    const valueBox = document.createElement('div');
+   valueBox.classList.add('box');
+   let shownValue = i;
+   if (i % 3 === 0 && i % 5 === 0) {
+      shownValue = 'FizzBuzz';
+      valueBox.classList.add('box-fizz-buzz');
+   } else if (i % 3 === 0) {
+      shownValue = 'Fizz';
+      valueBox.classList.add('box-fizz');
+   } else if (i % 5 === 0) {
+      shownValue = 'Buzz';
+      valueBox.classList.add('box-buzz');
+   }
+
    fizzBuzzBox.append(valueBox);
    valueBox.innerHTML = shownValue;
-
-   console.log(shownValue);
 }
