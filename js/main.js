77ce6a1f4fd128891bf872e2415bff13584725cf
dav100ide <oreglio.dev@gmail.com>
2022-10-25 10:48:17 +0200

    correzione const fuori dal ciclo

--- js/main.js
+++ js/main.js
@@ -1,11 +1,12 @@
+const fizzBuzzBox = document.querySelector('.fizz-buzz-box');
 // ciclo a contatore, 100 volte a partire da 1
 for (let i = 1; i <= 100; i++) {
-   // stampo i numeri da 1 a 100; ma i multipli di 3 = "fizz" & i multiplo di 5 = "buzz" & i multipli di 15 = "fizzBuzz"
+   // stampo i numeri da 1 a 100; ma i multipli di 3 = "fizz" & i multipli di 5 = "buzz" & i multipli di 15 = "fizzBuzz"
    // creo tanti box quanti sono sono gli shownValue
-   const fizzBuzzBox = document.querySelector('.fizz-buzz-box');
    const valueBox = document.createElement('div');
    valueBox.classList.add('box');
    let shownValue = i;
+
    if (i % 3 === 0 && i % 5 === 0) {
       shownValue = 'FizzBuzz';
       valueBox.classList.add('box-fizz-buzz');
